69504e06049ab7dda65720fd424006747d7b1ba1
jay335 <pjay22385@gmail.com>
2025-10-14 21:31:36 +0530

    added cidr

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -15,8 +15,8 @@ data "aws_availability_zones" "available" {
 
 # --- Public Subnets ---
 resource "aws_subnet" "public" {
-  cidr_block              = "10.0.1.0/24"
   count                   = 2
+  cidr_block              = "10.0.${count.index + 8}.0/24"
   vpc_id                  = aws_vpc.app_vpc.id
   availability_zone       = data.aws_availability_zones.available.names[count.index]  # Use count.index instead of count
   map_public_ip_on_launch = true
